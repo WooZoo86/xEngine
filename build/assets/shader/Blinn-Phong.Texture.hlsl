0d9f7c262d04f53402d339bd815db0c50eaa9d24
leafnsand <xavierjiang@hotmail.com>
2017-10-11 19:21:50 +0800

    fixed shader

--- build/assets/shader/Blinn-Phong.Texture.hlsl
+++ build/assets/shader/Blinn-Phong.Texture.hlsl
@@ -8,9 +8,9 @@ struct VS_INPUT
 struct VS_OUTPUT
 {
     float4 Position : SV_Position;
-    float3 ViewPosition : POSITION;
     float2 Texcoord : TEXCOORD;
     float3 Normal : NORMAL;
+    float3 PixelPosition : POSITION;
 };
 
 #define PS_INPUT VS_OUTPUT
@@ -30,15 +30,17 @@ SamplerState uTexture_sampler;
 VS_OUTPUT VS(VS_INPUT input)
 {
     VS_OUTPUT output;
-    
-    float4x4 MV_matrix = mul(uView, uModel);
-    output.Normal = mul((float3x3)transpose(MV_matrix), input.aNormal);
 
-    float4 ViewVector = mul(MV_matrix, float4(input.aPosition, 1.0));
-    output.ViewPosition = ViewVector.xyz;
+    // float3 aBinormal = cross(input.aTangent, input.aNormal);
+    // float3 T = normalize(mul(uModel, float4(input.aTangent,   0.0)).xyz);
+    // float3 B = normalize(mul(uModel, float4(aBinormal,        0.0)).xyz);
+    // float3 N = normalize(mul(uModel, float4(input.aNormal,    0.0)).xyz);
+    // float3x3 TBN = float3x3(T, B, N);
     
+    output.PixelPosition = mul(uModel, float4(input.aPosition, 1.0)).xyz;
+    output.Position = mul(uProjection, mul(uView, float4(output.PixelPosition, 1.0)));
+    output.Normal = mul(uModel, float4(input.aNormal, 0.0)).xyz;
     output.Texcoord = input.aTexcoord0;
-    output.Position = mul(uProjection, ViewVector);
     
     return output;
 }
@@ -47,21 +49,23 @@ PS_OUTPUT PS(PS_INPUT input)
 {
     PS_OUTPUT output;
 
-    float3 light_color = float3(1.0, 1.0, 1.0);
+    float ambientStrength = 0.1;
+    float diffuseStrength = 0.5;
+    float specularStrength = 0.4;
 
-    float3 light_direction = normalize(float3(-1.0, 0.0, -1.0));
+    float3 lightPos = float3(0.0, 0.0, 10.0);
+    float3 viewPos = float3(0.0, 0.0, 10.0);
+    float3 lightColor = float3(1.0, 1.0, 1.0);
 
+    float3 viewDir = normalize(viewPos - input.PixelPosition.xyz);
+    float3 lightDir = normalize(lightPos - input.PixelPosition.xyz);
     float3 normal = normalize(input.Normal);
 
-    float3 ambient = 0.1 * light_color;
+    float3 ambient = ambientStrength * lightColor;
+    float3 diffuse = diffuseStrength * max(dot(normal, lightDir), 0.0) * lightColor;
+    float3 specular = specularStrength * pow(max(dot(viewDir, reflect(-lightDir, normal)), 0.0), 32) * lightColor;
 
-    float3 diffuse = 0.5 * max(dot(normal, -light_direction), 0.0) * light_color;
-
-    float3 specular = 0.6 * pow(max(dot(normal, normalize((-light_direction) + (-input.ViewPosition))), 0.0), 16) * light_color;
-
-    float3 result = ambient + diffuse + specular;
-
-    output.Color = float4(result, 1.0) * uTexture.Sample(uTexture_sampler, input.Texcoord);
+    output.Color = float4(ambient + diffuse + specular, 1.0) * uTexture.Sample(uTexture_sampler, input.Texcoord);
 
     return output;
 }
